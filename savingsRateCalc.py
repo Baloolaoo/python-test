08c7e79da7e5dc9b447533bbb3caa27865fb4827
KUND, Hendrik - Desktop <rickikund@gmail.com>
2021-03-03 15:42:00 +0100

    Refactoring print text; removed index-call from month variable

--- savingsRateCalc.py
+++ savingsRateCalc.py
@@ -3,14 +3,16 @@ list_of_returns = [0.60, -7.78, -13.37, 11.00, 3.03, 1.52, -0.57, 5.33, -1.68, -
 #savings rate per month in EUR
 monthly_savings_rate = 100.0
 
+month = 1
 total_amount = 0.0
 
 for x in list_of_returns:
-    month = list_of_returns.index(x) + 1
     total_amount += monthly_savings_rate
     total_amount += total_amount * x / 100
 
-    print(str(month) + ":\t " + str(total_amount) + "\t r=\t" + str(x))
+    temp_txt = '{m}: \t {ta:.2f} \t r= \t {r:.2f}'
+    print(temp_txt.format(m=month, ta=total_amount, r=x))
+    month += 1
 
-txt = "Total: \t {temp:.2f}"
+txt = 'Total: \t {temp:.2f}'
 print(txt.format(temp=total_amount))
