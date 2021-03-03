618619fbee34161875e8c843ac93640fef0e5601
KUND, Hendrik - Desktop <rickikund@gmail.com>
2021-03-03 18:52:19 +0100

    Changed monthly_savings_rate to console input

--- savingsRateCalc.py
+++ savingsRateCalc.py
@@ -1,7 +1,8 @@
 #monthly returns of MSCI World (2020)
 list_of_returns = [0.60, -7.78, -13.37, 11.00, 3.03, 1.52, -0.57, 5.33, -1.68, -2.49, 9.70, 1.81]
 #savings rate per month in EUR
-monthly_savings_rate = 100.0
+
+monthly_savings_rate = float(input('Enter monthly savings rate: '))
 
 month = 1
 total_amount = 0.0
@@ -16,3 +17,4 @@ for x in list_of_returns:
 
 txt = 'Total: \t {temp:.2f}'
 print(txt.format(temp=total_amount))
+print('Savings: \t' + str((month - 1) * monthly_savings_rate))
