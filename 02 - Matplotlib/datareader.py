5157488707c376bdaa42c6d16997b0413bd376dc
KUND, Hendrik - Desktop <rickikund@gmail.com>
2021-03-09 19:06:01 +0100

    New 'difference' column in dataset

--- 02 - Matplotlib/datareader.py
+++ 02 - Matplotlib/datareader.py
@@ -14,4 +14,16 @@ end_date = '2021-02-28'
 # Call the function DataReader from the class data
 dataset = data.DataReader('GC=F', 'yahoo', start_date, end_date)
 
+# Append a new column with difference to previous day
+dataset['difference'] = dataset['Close'].diff()
+
 print(dataset)
+
+temp_counter = 0
+for x in dataset['difference']:
+    if x > 0: temp_counter += 1
+
+print('')
+print('Succ: ' + str(temp_counter))
+print('Loss: ' + str(len(dataset)-temp_counter))
+print('Totl: ' + str(len(dataset)))
