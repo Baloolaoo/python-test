93ea837375b8f051ab2ae5a91a773768f554a48b
KUND, Hendrik - Desktop <rickikund@gmail.com>
2021-03-09 14:57:42 +0100

    Refactoring

--- 02 - Matplotlib/datareader.py
+++ 02 - Matplotlib/datareader.py
@@ -1,8 +1,8 @@
-# loading the class data from the package pandas_datareader
-#from pandas_datareader import data
-
+# imports
 import pandas_datareader as data
 import pandas as pd
+
+# set print options for console output
 pd.set_option('max_rows', 1000)
 pd.set_option('max_columns', 1000)
 pd.set_option('display.width', 1000)
@@ -12,6 +12,6 @@ start_date = '2020-01-01'
 # Last day
 end_date = '2021-02-28'
 # Call the function DataReader from the class data
-goog_data = data.DataReader('GC=F', 'yahoo', start_date, end_date)
+dataset = data.DataReader('GC=F', 'yahoo', start_date, end_date)
 
-print(goog_data)
+print(dataset)
