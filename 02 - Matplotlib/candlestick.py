b74d2a9f8825bd375e59d27fed52b6d64d6fd2a0
KUND, Hendrik - Desktop <rickikund@gmail.com>
2021-03-18 11:29:12 +0100

    Added col for upday in dataset

--- 02 - Matplotlib/candlestick.py
+++ 02 - Matplotlib/candlestick.py
@@ -9,11 +9,24 @@ import mplfinance as mpf
 This scripts purpose is to plot a candlestick chart
 '''
 
+# set print options for console output
+pd.set_option('max_rows', 1000)
+pd.set_option('max_columns', 1000)
+pd.set_option('display.width', 1000)
+
 # First day
-start_date = '2020-02-01'
+start_date = '2021-01-01'
 # Last day
 end_date = '2021-02-28'
 # Call the function DataReader from the class data
 dataset = data.DataReader('GC=F', 'yahoo', start_date, end_date)
+
+for i, row in dataset.iterrows():
+    upday_val = False
+    if row['Close'] > row['Open']:
+        upday_val = True
+    dataset.at[i,'Upday'] = upday_val
+
+print(dataset)
 
 mpf.plot(dataset,type='candle')
