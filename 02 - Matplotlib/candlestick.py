3f896591bc262905afd183ba04ce474fdb5fb00f
KUND, Hendrik - Desktop <rickikund@gmail.com>
2021-03-23 14:47:15 +0100

    Moved upday-loop to separate file tradingfunctions.py

--- 02 - Matplotlib/candlestick.py
+++ 02 - Matplotlib/candlestick.py
@@ -1,13 +1,12 @@
 # imports
 import pandas_datareader as data
 import pandas as pd
-import numpy as np
 import mplfinance as mpf
+import tradingfunctions as tf
 
 '''
 This scripts purpose is to plot a candlestick chart out of a pandas dataframe.
 Next To-Do:
-    - Move 'upday'-loop to function
     - Add functions for
         - Inside days
         - Outside days
@@ -26,15 +25,8 @@ end_date = '2021-02-28'
 # Call the function DataReader from the class data
 dataset = data.DataReader('GC=F', 'yahoo', start_date, end_date)
 
-# Upday (Close > Open)
-for i, row in dataset.iterrows():
-    upday_val = np.nan
-    if (row['Close'] > row['Open']):
-        upday_val = row['Low']*0.99
-        dataset.at[i, 'Upday'] = True
-        dataset.at[i,'Upday_Signal'] = upday_val
-    else:
-        dataset.at[i, 'Upday'] = False
+# Transform with tradingfunctions.py
+dataset = tf.updays(dataset)
 
 apd = mpf.make_addplot(dataset['Upday_Signal'], type='scatter',markersize=100,marker='^', color='r')
 
